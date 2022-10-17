3229fe14f5ae0ea07ae769ecdbfe5a42bd04d3c1
James-brian <brianoigo7854@gmail.com>
2022-10-17 09:22:02 -0700

    program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,13 +4,11 @@
 
 #include <stdio.h>
 
-/**                                                                                                                                                           
-* main - Entry point                                                                                                                                   
-*                                                                                                                         
-* Description: print value of n satus; zero, positive or negative
-*
-* Return: Always 0 (Success)
-*/ 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
 
 
 int main(void)
