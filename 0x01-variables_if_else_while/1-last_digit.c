d022436b08b6236c582e871ca00be6cda4872b73
James-brian <brianoigo7854@gmail.com>
2022-10-17 07:07:24 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,14 +4,12 @@
 
 #include <stdio.h>
 
-
-
 /**
- *main - Entry point
+ * main - Entry point
  *
- * Description: print the value of n status:greater than, is zero and is not less than 6.
+ * Description: print value of n satus; greater than, is zero and is not less than 6.
  *
- * Return: Always O (Success)
+ *Return: Always 0 (Success)
  */
 
 
