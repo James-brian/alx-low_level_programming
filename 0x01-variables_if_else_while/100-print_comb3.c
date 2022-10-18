574442af9a63038582c9be5b983571bb38af72c0
James-brian <brianoigo7854@gmail.com>
2022-10-18 00:36:48 -0700

    prints all possible different combinations of two digits.
    Numbers must be separated by ,, followed by a space
    
    The two digits must be different
    
    01 and 10 are considered the same combination of the two digits 0 and 1
    
    Print only the smallest combination of two digits
    
    Numbers should be printed in ascending order, with two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,23 @@
 
 int main(void)
 {
-int num1;
-int num2;
-for (num1 = 48; num2 <= 78; num1++)
+int num1 = 0, num2;
+while (num1 <= 9){
+num2 = 0;
+while (num2 <= 9){
+if (num1 != num2 && num1 < num2)
+																				{
+putchar(num1 + 48)
+putchar(num2 + 48)
+if (num1 + num2 != 17)
 {
-for (num2 = num1 + 1 ; num2 <= 78; num2++)
-{
-putchar(num1);
-putchar (num2);
-if ((num1 == 56) && (num2 == 78))
-{
-break;
-}
 putchar(',');
-putchar (' ');
+																				putchar(' ');
+																				}
+
 }
+++num2;																				}
+++num1;
 }
 putchar('\n');
 return (0);
