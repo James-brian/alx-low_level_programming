5ddf080f998612a81b9fff9517c9a727e275acd0
James-brian <brianoigo7854@gmail.com>
2022-10-21 00:18:03 -0700

    function that multiplies two integers.
    Prototype: int mul(int a, int b);

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+* mul - returns product of its parameters
+*
+* @x: integer type number
+*
+* @y: integer type number
+*
+* Return:0
+*/
+
+
+int mul(int a, int b)
+
+{
+return (a * b);
+}
