be34d3ce3d9769799f16bf638b6f806ecefc81f6
James-brian <brianoigo7854@gmail.com>
2022-10-27 06:10:43 -0700

    function that encodes a string into 1337.
    Letters a and A should be replaced by 4
    
    Letters e and E should be replaced by 3
    
    Letters o and O should be replaced by 0
    
    Letters t and T should be replaced by 7
    
    Letters l and L should be replaced by 1
    
    Prototype: char *leet(char *);
    
    You can only use one if in your code
    
    You can only use two loops in your code
    
    You are not allowed to use switch
    
    You are not allowed to use any ternary operation

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+
+/**
+ * leet - transform to leet
+ *
+ * @str: char array string type
+ *
+ * Return: str transformed
+ */
+
+char *leet(char *str)
+
+{
+
+	int x = 0, y;
+
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[++x])
+
+	{
+
+		for (y = 0; y <= 7; y++)
+
+		{
+
+			if (str[x] == leet[y] || str[x] - 32 == leet[y])
+
+			{
+
+				str[x] = y + '0';
+
+			}
+
+		}
+
+	}
+
+	return (str);
+
+}
