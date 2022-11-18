737cc1f65910d7965660a45d50bff6f1ea3661b0
James-brian <brianoigo785@gmail.com>
2022-11-18 02:18:07 -0800

    function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,8 +14,8 @@ void print_all(const char * const format, ...)
 	unsigned int x = 0, y, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
-	va_start(valist, format);
 
+	va_start(valist, format);
 	while (format && format[x])
 	{
 		y = 0;
@@ -32,15 +32,12 @@ void print_all(const char * const format, ...)
 			case 'c':
 				printf("%c", va_arg(valist, int)), c = 1;
 				break;
-
 			case 'i':
 				printf("%d", va_arg(valist, int)), c = 1;
 				break;
-
 			case 'f':
 				printf("%f", va_arg(valist, double)), c = 1;
 				break;
-
 			case 's':
 				str = va_arg(valist, char *), c = 1;
 				if (!str)
